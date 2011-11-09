bf52a1542b0a907c4a78fa46a7451d58df65e12e
Adam Wick <awick@uhsure.com>
2011-11-08 22:44:18 -0500

    More channel examples! (That work!)

---
+++ tests/test.chan3.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <thread.h>
+
+channel *chan;
+
+void sender(void *dead)
+{
+  unsigned long err, i;
+
+  for(i = 0; i < 10; i++) {
+      if((err = send_channel(chan, (void*)i)) != 0) {
+        printf("Failure to send! (%i)\n", err);
+      }
+  }
+}
+
+void receiver(void *dead)
+{
+  void *val;
+  unsigned long err, i;
+
+  for(i = 0; i < 10; i++) {
+      if((err = recv_channel(chan, &val)) != 0) {
+        printf("Failure to receive! (%i)\n", err);
+      } else {
+        printf("Received value 0x%lx\n", (unsigned long)val);
+      }
+      if((unsigned long)val != i) printf("Bad value!\n");
+  }
+}
+
+void initThread(void *dead)
+{
+    chan = create_channel();
+    create_thread(receiver, (void*)0, 10240);
+    sender(0);
+    yield();
+}
+
+int main(int argc, char **argv)
+{
+    run_threaded_system(initThread, (void*)0, 10240);
+    return 0;
+}
---
+++ tests/test.chan5.c
@@ -0,0 +1,69 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <thread.h>
+
+void sender(channel *outchan)
+{
+  unsigned long err, i;
+
+  for(i = 0; i < 100; i++) {
+      if((err = send_channel(outchan, (void*)i)) != 0) {
+        printf("Failure to send! (%i)\n", err);
+      }
+  }
+}
+
+typedef struct dual_channel {
+  channel *in, *out;
+} dual_channel;
+
+void transfer(dual_channel *chans)
+{
+    unsigned long err, i, val;
+
+    for(i = 0; i < 100; i++) {
+        if((err = recv_channel(chans->in, (void**)&val)) != 0) {
+          printf("Failure to receive! (%i)\n", err);
+        } else {
+          if((err = send_channel(chans->out, (void*)val)) != 0) {
+            printf("Failure to send! (%i)\n", err);
+          }
+        }
+    }
+}
+
+void receiver(channel *inchan)
+{
+  unsigned long err, i;
+  void *val;
+
+  for(i = 0; i < 100; i++) {
+      if((err = recv_channel(inchan, &val)) != 0) {
+        printf("Failure to receive! (%i)\n", err);
+      } else {
+        printf("Received value %ld\n", (unsigned long)val);
+      }
+      if((unsigned long)val != i) printf("Bad value!\n");
+  }
+}
+
+void initThread(void *dead)
+{
+    channel *prev = create_channel();
+    int i;
+
+    create_thread((task)sender,   (void*)prev, 10240);
+    for(i = 0; i < 10; i++) {
+        dual_channel *dual = malloc(sizeof(dual_channel));
+        dual->in  = prev;
+        dual->out = prev = create_channel();
+        create_thread((task)transfer, (void*)dual, 10240);
+    }
+    create_thread((task)receiver, (void*)prev, 10240);
+}
+
+int main(int argc, char **argv)
+{
+    run_threaded_system(initThread, (void*)0, 10240);
+    return 0;
+}
